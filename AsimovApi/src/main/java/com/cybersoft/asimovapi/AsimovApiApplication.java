6533971ed58363e2972e16dae46a6311f0d32c0a
JoaquinSM13 <u201910511@upc.edu.pe>
2022-06-04 19:51:02 -0500

    feat: base project

---
+++ AsimovApi/src/main/java/com/cybersoft/asimovapi/AsimovApiApplication.java
@@ -0,0 +1,13 @@
+package com.cybersoft.asimovapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsimovApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsimovApiApplication.class, args);
+    }
+
+}
---
+++ AsimovApi/src/test/java/com/cybersoft/asimovapi/AsimovApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.asimovapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsimovApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
