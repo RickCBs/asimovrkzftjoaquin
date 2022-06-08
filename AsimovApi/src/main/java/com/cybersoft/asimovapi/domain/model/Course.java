23238d5e7b11086f8641bc19cd5eff93f5648c8a
RickCB <erikc.cortezb@gmail.com>
2022-06-07 20:33:32 -0500

    feat: repository and service

---
+++ AsimovApi/src/main/java/com/cybersoft/asimovapi/domain/model/Course.java
@@ -0,0 +1,29 @@
+package com.cybersoft.asimovapi.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    @NotBlank
+    private String description;
+    @NotNull
+    @NotBlank
+    private boolean state;
+}
